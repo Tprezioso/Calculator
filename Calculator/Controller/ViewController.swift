57b715d16aa530cc8cedad235164f9b28fbf56aa
Tprezioso <tommyprezioso@gmail.com>
2019-01-11 19:23:22 -0500

    working on state of when numbers are pressed

--- Calculator/Controller/ViewController.swift
+++ Calculator/Controller/ViewController.swift
@@ -12,19 +12,30 @@ class ViewController: UIViewController {
     
     @IBOutlet weak var displayLabel: UILabel!
     
+    var isFinishedTypingNumber : Bool = true
+    
     
     
     @IBAction func calcButtonPressed(_ sender: UIButton) {
         
         //What should happen when a non-number button is pressed
-    
+        isFinishedTypingNumber = true
+
     }
 
     
     @IBAction func numButtonPressed(_ sender: UIButton) {
         
         //What should happen when a number is entered into the keypad
-    
+        if let numValue = sender.currentTitle {
+            
+            if isFinishedTypingNumber {
+                isFinishedTypingNumber = false
+            } else if !isFinishedTypingNumber {
+                displayLabel.text = displayLabel.text! + numValue
+            }
+            
+        }
     }
 
 }
